3db589832b0d75afdc074d59dbdf92f12875b382
Raul <raulvillajr95@gmail.com>
2022-05-18 21:52:31 -0700

    Create random choice for 'rock', 'paper', or 'scissors'

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello sone')
+// random number between 1 and b
+function randomNum(b) {
+  return Math.floor(Math.random() * b) + 1;
+}
+
+function computerPlay() {
+  let computerChoice = randomNum(3)
+  if (computerChoice == 1) {
+    return 'Rock'.toLowerCase()
+  } else if (computerChoice == 2) {
+    return 'Paper'.toLowerCase()
+  } else {
+    return 'Scissors'.toLowerCase()
+  }
+
+}
+
+function oneRoundRPS(playerSelection, computerSelection) {
+  let playersChoice = playerSelection.toLowerCase()
+  let computersChoice = computerSelection
+
+  return `${playersChoice}, ${computersChoice}`
+}
+
+console.log(oneRoundRPS('RocK', computerPlay()))
